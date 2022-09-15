6d79fa70ff30713298cdc50c3bb0b975be288729
olberd <olberd@bk.ru>
2022-09-15 21:04:53 +0300

    Add fetch_spacex_last_launch for loading all launch photos

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from pathlib import Path
 
-headers = {'User-Agent': 'CoolBot/0.0 (https://example.org/coolbot/; coolbot@example.org)'}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'}
 
 def save_image(url, filename):
     response = requests.get(url, headers=headers)
@@ -20,9 +20,26 @@ def get_links_photo_spacex(link_launch):
     return photo_urls
 
 
-name_photo = 'hubble.jpeg'
-url_photo = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-save_image(url_photo, name_photo)
-# url = 'https://api.spacexdata.com/v5/launches/latest'
-url_launch = 'https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a'
-print(get_links_photo_spacex(url_launch))
+def fetch_spacex_last_launch(url_launch):
+    links_photo_spacex = get_links_photo_spacex(url_launch)
+    for num, photo_spacex in enumerate(links_photo_spacex):
+        file = f"spacex_{num}.jpg"
+        save_image(photo_spacex, file)
+
+        print(file)
+
+
+def main():
+    fetch_spacex_last_launch('https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a')
+
+
+# name_photo = 'hubble.jpeg'
+# url_photo = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+# save_image(url_photo, name_photo)
+# # url = 'https://api.spacexdata.com/v5/launches/latest'
+# url_launch = 'https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a'
+# print(get_links_photo_spacex(url_launch))
+
+
+if __name__ == "__main__":
+    main()
