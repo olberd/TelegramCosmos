790cd327180ccdbb5babc074e1fb23e3a727cb91
olberd <olberd@bk.ru>
2022-09-19 20:56:01 +0300

    publish photo to Telegram channel.

--- cosmos_bot.py
+++ cosmos_bot.py
@@ -9,7 +9,7 @@ load_dotenv()
 api_telegram = os.environ['API_TELEGRAM']
 bot = telegram.Bot(token=api_telegram)
 chat_id = '@tk_elbrus'
-# bot.send_message(chat_id='@tk_elbrus', text="Hi!")
+bot.send_message(chat_id='@tk_elbrus', text="Hi!")
 bot.send_document(chat_id=chat_id, document=open('image/nasa_apod_8.jpg', 'rb'))
 
 
